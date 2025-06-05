e517e47d86bd275a75b2e1b73ac2333fab206d3c
itspacrat <blakemichaelgaynor@gmail.com>
2025-06-05 15:31:57 -0700

    refactor GetUsers into lambda, add SaveUsers method

--- Authorizer/UserDB.cs
+++ Authorizer/UserDB.cs
@@ -4,33 +4,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
-
 namespace Authorizer
 {
     public class UserDB
     {
-
+        /// <summary>
+        /// lambda that retrieves the users from the stored file into a List
+        /// </summary>
+        public List<string> GetUSers =>
+            System.IO.File.ReadAllText(Authorizer.UsersPath).Trim().Split('|').ToList();
+        
+        /// <summary>
+        /// write a list of user strings back to the stored file (overwrite)
+        /// </summary>
+        /// <param name="users"></param>
+        public void SaveUsers(List<string> users)
+        {
+            System.IO.File.WriteAllText(Authorizer.UsersPath, string.Join("|", users));
+        }
+        /// <summary>
+        /// empty constructor for serialization
+        /// </summary>
         public UserDB() { 
            
         }
-
-        public static List<List<string>> GetUsers()
-        {
-            var users = System.IO.File.ReadAllText(Authorizer.UsersPath).Trim().Split('|').ToList();
-
-            List<List<string>> userinfo = new List<List<string>>();
-
-            foreach (var user in users){
-                
-                List<string> userInfo = user.Split('\n').ToList();
-
-            }
-          
-            return userinfo;
-        }
-
-  
-
-
     }
 }
