71a1a6af7bd0b24ca760395d8491ad8a0ac7121c
Travis <146320687+UnderpaidCoder@users.noreply.github.com>
2025-06-04 20:41:49 -0700

    started new auth thing

--- Authorizer/Authorizer.cs
+++ Authorizer/Authorizer.cs
@@ -1,5 +1,34 @@
-﻿namespace Authorizer {
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+
+
+
+
+
+
+namespace Authorizer {
     public class  Authorizer {
+      
+        public static Dictionary<char, int> encryptionMap = new Dictionary<char, int>();
+
+        Random rand = new Random();
+        static void Main() { 
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+
+
+
         public static string GetKey(string input) {
             // Creates a monosub key mapping for the given input
             return "placeholder";
