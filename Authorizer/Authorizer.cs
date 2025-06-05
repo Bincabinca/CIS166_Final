508347c6dfad1f63a028f38417b73e53d9004df7
Travis <146320687+UnderpaidCoder@users.noreply.github.com>
2025-06-04 21:24:16 -0700

    scaffolding for auth

--- Authorizer/Authorizer.cs
+++ Authorizer/Authorizer.cs
@@ -11,23 +11,38 @@ using System;
 
 namespace Authorizer {
     public class  Authorizer {
-      
-        public static Dictionary<char, int> encryptionMap = new Dictionary<char, int>();
+        
+        private const string UsersPath = @"..\..\DataFiles\UserDB.txt";
 
+
+
+        //private Dictionary<string, List<int>> users = new Dictionary<string, List<int>>();
+        //private Dictionary<char, int> passwordMappings = new Dictionary<char,int>();
         Random rand = new Random();
-        static void Main() { 
-            
         
-        
-        
-        
-        
-        
+     
+
+        public static void saveUser(string username) { 
         
         
         }
+      
+        //private List<int> encryptPassword(string password) {
+        //    // Encrypts the password using a simple mapping
+        //    List<int> encryptedPass = new List<int>();
+        //    foreach (char c in password) {
 
+        //        if (passwordMappings.ContainsKey(c)) {
 
+        //            encryptedPass.Add(passwordMappings[c]);
+        //        } else {
+        //            int newValue = rand.Next(1,25); // Random value for simplicity
+        //            passwordMappings[c] = newValue;
+        //            encryptedPass.Add(newValue);
+        //        }
+        //    }
+        //    return encryptedPass;
+        //}
 
         public static string GetKey(string input) {
             // Creates a monosub key mapping for the given input
