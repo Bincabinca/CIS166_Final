1778a400b6fa998e01d6deeb3e3c064f78f59140
itspacrat <blakemichaelgaynor@gmail.com>
2025-06-04 20:31:50 -0700

    authorizer established

---
+++ Authorizer/Authorizer.cs
@@ -0,0 +1,10 @@
+﻿namespace Authorizer {
+    public class  Authorizer {
+        public static string GetKey(string input) {
+            // Creates a monosub key mapping for the given input
+            return "placeholder";
+        }
+    }
+        
+    }
+
