526a3fcd1faee7bba07719141eefb37467cb0f9d
Travis <146320687+UnderpaidCoder@users.noreply.github.com>
2025-06-09 19:47:19 -0700

    Started stinky comments

---
+++ CarDealership/CommentStorage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership
+{
+    public static class CommentStorage
+    {
+        private static Dictionary<int,List <String>> 
+            commentsData = new Dictionary<int, List<string>>();
+
+        public static void addComment(int ListingID, string comment)
+        { 
+            if(!commentsData.ContainsKey(ListingID))
+            {
+                commentsData[ListingID] = new List<string>();
+            }
+            commentsData[ListingID].Add(comment);
+        }
+        public static List<string> getComments(int ListingID)
+        {
+            if (commentsData.ContainsKey(ListingID))
+            {
+                return commentsData[ListingID];
+            }
+            return new List<string>(); // Return an empty list if no comments found for the ListingID
+        }
+
+    }
+}
